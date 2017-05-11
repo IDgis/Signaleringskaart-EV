52891da74bfb37df1ecd8e78f192eab54358498a
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2017-05-11 15:30:39 +0200

    Removed unused imports

--- veiligheidstoetsservlet/src/main/java/nl/prv/veiligheidstoets/util/TemplateHandler.java
+++ veiligheidstoetsservlet/src/main/java/nl/prv/veiligheidstoets/util/TemplateHandler.java
@@ -1,8 +1,6 @@
 package nl.prv.veiligheidstoets.util;
 
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import org.antlr.stringtemplate.StringTemplate;
 import org.antlr.stringtemplate.StringTemplateGroup;
