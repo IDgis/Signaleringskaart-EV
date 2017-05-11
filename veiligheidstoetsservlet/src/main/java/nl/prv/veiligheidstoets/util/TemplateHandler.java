cee2fbcecaf50353ee235d1a6555a42323e2ed65
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2017-05-11 15:28:25 +0200

    Removed unnecessary use of iterator

--- veiligheidstoetsservlet/src/main/java/nl/prv/veiligheidstoets/util/TemplateHandler.java
+++ veiligheidstoetsservlet/src/main/java/nl/prv/veiligheidstoets/util/TemplateHandler.java
@@ -17,13 +17,7 @@ public class TemplateHandler {
 	}
 	
 	public String getFilter(String templateName, Map<String,String> props){
-		StringTemplate template = templateGroup.getInstanceOf(templateName);
-		Iterator<Entry<String, String>> it = props.entrySet().iterator();
-		while (it.hasNext()) {  				 
-		    Map.Entry<String,String> pairs = it.next();
-		    template.setAttribute(pairs.getKey(), pairs.getValue());  
-		    //it.remove(); // avoids a ConcurrentModificationException
-		}
+		StringTemplate template = templateGroup.getInstanceOf(templateName, props);
 		return template.toString();
 	}
 }
