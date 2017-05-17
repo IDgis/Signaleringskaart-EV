ee62d103152493e571a96f8990d057cc76e1be4a
LindaVels <linda.vels@idgis.nl>
2017-05-17 15:58:11 +0200

    create script voor lege services tabellen

--- docker/database/000_create_db.sql
+++ docker/database/000_create_db.sql
@@ -1,9 +1,10 @@
+create database ev_lime_ov;
+\c ev_lime_ov
+
+create extension postgis;
+
 create database ev_services_ov;
 \c ev_services_ov
 
 create extension postgis;
 
-create database ev_lime_ov;
-\c ev_lime_ov
-
-create extension postgis;
